ab90b9c8a75d0a3409f9ebd78fd0ffc4d756b1cc
Mesut YILMAZ <yilmaz.mesut@windowslive.com>
2022-09-09 22:42:39 +0300

    Update CouchbaseConfiguration.java

--- src/main/java/com/todo/app/config/CouchbaseConfiguration.java
+++ src/main/java/com/todo/app/config/CouchbaseConfiguration.java
@@ -4,9 +4,12 @@ import com.couchbase.client.java.env.ClusterEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+import sun.tools.java.ClassPath;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -53,12 +56,8 @@ public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
     @Override
     protected void configureEnvironment(final ClusterEnvironment.Builder builder) {
         try {
-            URL systemResource = ClassLoader.getSystemResource("capella.pem");
-            URL applicationyml = ClassLoader.getSystemResource("application.properties");
-            log.error("CAPELLA.PEM -> " + systemResource);
-            log.error("application.properties -> " + applicationyml);
-            builder.securityConfig().enableTls(true).trustCertificate(Paths.get(systemResource.toURI()));
-        } catch (URISyntaxException e) {
+            builder.securityConfig().enableTls(true).trustCertificate(Paths.get(new ClassPathResource("capella.pem").getURI()));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
